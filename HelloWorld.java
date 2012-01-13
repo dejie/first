8d71f144042333e8b4c937e5809516b724a60608
cdjflxk <736233152@qq.com>
2012-01-13 10:48:42 +0800

    create the HelloWorld class

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2011 Talend – www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+
+/**
+ * DOC Administrator class global comment. Detailled comment <br/>
+ * 
+ * $Id: talend.epf 55206 2011-02-15 17:32:14Z mhirt $
+ * 
+ */
+public class HelloWorld {
+
+    /**
+     * DOC Administrator Comment method "main".
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+
+}
